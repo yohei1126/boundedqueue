51dbeeb48499e77f0617a14c83e9071c2b8f9d92
Yohei Onishi <vivre214@gmail.com>
2011-10-01 22:52:31 +0900

    add files for Step3

---
+++ Step3/BoundedQueue.h
@@ -0,0 +1,15 @@
+#ifndef __bounded_queue_h_
+#define __bounded_queue_h_
+
+#include "QueueControl.h"
+
+template <typename T>
+class BoundedQueue {
+ public:
+  BoundedQueue(QueueControl& producer, QueueControl& consumer){
+    producer.Resume();
+    consumer.Resume();
+  }
+};
+
+#endif
---
+++ Step3/QueueControl.h
@@ -0,0 +1,11 @@
+#ifndef __QUEUE_CONTROL_H_
+#define __QUEUE_CONTROL_H_
+
+class QueueControl {
+public:
+	virtual ~QueueControl() {}
+	virtual void Resume() = 0;
+	virtual void Pause()  = 0;
+};
+
+#endif
