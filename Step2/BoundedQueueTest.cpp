e9005446f506f8abc6aa30c642a8822f73829da5
Yohei Onishi <vivre214@gmail.com>
2011-10-01 22:50:59 +0900

    add files for step2

---
+++ Step2/BoundedQueueTest.cpp
@@ -0,0 +1,28 @@
+#include "BoundedQueue.h"
+
+#include <gmock/gmock.h>
+
+class MockQueueControl : public QueueControl {
+public:
+  MOCK_METHOD0(Resume, void());
+  MOCK_METHOD0(Pause,  void());
+};
+
+/*
+class BoundedQueueTest : public ::testing::Test {
+public:
+
+};
+*/
+
+TEST(BoundedQueueTest, CreateResumesProducer) {
+  MockQueueControl producer, consumer;
+  EXPECT_CALL(producer, Resume());
+  BoundedQueue<int> q(producer, consumer);
+}
+
+TEST(BoundedQueueTest, CreatePausesConsumer){
+  MockQueueControl producer, consumer;
+  EXPECT_CALL(consumer, Resume());
+  BoundedQueue<int> q(producer, consumer);
+}
