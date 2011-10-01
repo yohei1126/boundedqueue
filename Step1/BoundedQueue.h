96014fe9c83d317f4aeac597d44276c35004a1ad
Yohei Onishi <vivre214@gmail.com>
2011-10-01 22:46:34 +0900

    add files for Step1

---
+++ Step1/BoundedQueue.h
@@ -0,0 +1,14 @@
+#ifndef __bounded_queue_h_
+#define __bounded_queue_h_
+
+#include "QueueControl.h"
+
+template <typename T>
+class BoundedQueue {
+ public:
+  BoundedQueue(QueueControl& producer){
+    producer.Resume();
+  }
+};
+
+#endif
